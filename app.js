b5df237c493c44dbc8ec20de093f2ad16c5b0fc3
captaincode0 <developerdiego0@gmail.com>
2017-06-13 19:07:38 -0500

    Added message processing to the bot

--- app.js
+++ app.js
@@ -39,3 +39,63 @@ app.get("/webhook", function(req, res){
 		res.sendStatus(403);
 	}
 });
+
+//post method to make a webhook of the page
+app.post("/webhook", function(req, res){
+	//check if the current object is one page subscription
+	if(req.body.object == "page"){
+		//iterate all the entries if there is more than one
+		req.body.entry.forEach(function(entry){
+			//iterate message events
+			entry.messaging.forEach(function(event){
+				if(event.postback)
+					processPostBack(event);
+			});
+		});
+	}
+});
+
+function processPostBack(event){
+	var sender_id = event.sender.id;
+	var data_payload = event.postback.payload;
+
+	if(data_payload === "Greeting"){
+		//make one async request
+		request({
+			url: "https://graph.facebook.com/v2.6/"+sender_id,
+			qs: {
+				access_token: process.env.PAGE_ACCESS_TOKEN,
+				fields: "first_name"
+			},
+			method: "GET"
+		}, function(error, res, body){
+			let greeting = "";
+
+			if(error)
+				console.log("[-] Error getting user name"+error);
+			else{
+				let json_res = JSON.parse(body);
+				name = json_res.first_name;
+				gretting = "Hi, "+name+", ";
+			}
+
+			let message = greeting+"my name is ZZ Moviez bot, i can tell you about recent movies, actors and directors";
+			sendMessage(sender_id, message);
+		});
+	}
+}
+
+function sendMessage(recipient_id, message){
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+		method: "POST",
+		json: {
+			recipient: {id: recipient_id},
+			message: message
+		}
+	}, function(error, response, body){
+		if(error)
+			console.log("[-] Error sending the message: "+response.error);
+	})
+}
