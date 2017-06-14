33e11b74e4b38abdddb4f165589e8b3e24fa60ba
captaincode0 <developerdiego0@gmail.com>
2017-06-13 22:27:40 -0500

    Fixed app bugs

--- models/movie.js
+++ models/movie.js
@@ -1,11 +1,11 @@
 var mongoose = require("mongoose");
 var mongo_schema = mongoose.Schema;
 
-var movie_schema = new Schema({
+var movie_schema = new mongoose.Schema({
 	user_id: {type: String},
 	title: {type: String},
 	date: {type: String},
-	year: {type: string},
+	year: {type: String},
 	cast: {type: String},
 	rating: {type: String},
 	poster_url: {type: String}
